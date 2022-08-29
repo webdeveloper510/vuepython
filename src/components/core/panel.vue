db34adc0b1bfdc8766c5e7a66c6e8f94518db388
Rekha Thakur <rekhathakur@datepalm.media>
2022-08-29 20:18:48 +0530

    build

--- src/components/core/panel.vue
+++ src/components/core/panel.vue
@@ -1,7 +1,7 @@
 <template>
 
     <v-card class="rounded-lg" :class="cls" elevation="0">
-      <div class="circle_bg"></div>
+      <div class="circle_bg"></div> 
       <div class="circle_bg2"></div>
       <div class="circle_bg3"></div>
       <v-img src="@/assets/circle_bg3.svg" class="circle_bg3"></v-img>
